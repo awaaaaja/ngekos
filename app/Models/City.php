d5c897c2643c9cfd22aa992e9e48c8c044a33321
awaaaaja <awaaaaja@gmail.com>
2025-05-04 13:31:19 +0700

    main

---
+++ app/Models/City.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class City extends Model
+{
+    use HasFactory, softDeletes;
+
+    protected $fillable = [
+        'image',
+        'name',
+        'slug'
+    ];
+
+    
+
+    public function boardingHouses()
+    {   
+        return $this->hasMany(BoardingHouse::class);
+    }
+
+}
